ecd7d9e7ddd926583c468bc013a71cb3865ac7b6
codemeleon <anmol.kiran@gmail.com>
2021-08-02 13:52:46 +0200

    removal django-nvd3 and django_tables2

--- Database/settings.py
+++ Database/settings.py
@@ -55,8 +55,8 @@ INSTALLED_APPS = [
     'MicroBiome',
     'leaflet',
     'djgeojson',
-    'django_nvd3',
-    'bootstrap4',
+    # 'django_nvd3',
+    # 'bootstrap4',
     'crispy_forms',  # TODO: Clean it
     'tags_input',  # TODO: REmove this while cleaning the code
     'django_tables2',
