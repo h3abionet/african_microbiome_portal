d39cc4c5b6ebc2df4f27447b031206ef36efcef5
codemeleon <anmol.kiran@gmail.com>
2022-05-04 15:00:21 +0200

    Cleaning old files

---
+++ MicroBiome/management/commands/clean_old.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+# https://pypi.org/project/django-crontab/
+
+from shutil import rmtree
+from glob import glob
+from django.conf import settings
+import datetime
+import os
+
+
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+
+    """Docstring for Command."""
+
+    def __init__(self):
+        """TODO: to be defined."""
+        BaseCommand.__init__(self)
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        """TODO: Docstring for function.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        today = datetime.datetime.today()
+
+        fold = f"{settings.STATIC_ROOT}/downloads"
+        print(fold)
+
+        for fl in glob(f"{fold}/*"):
+            print(fl)
+            modified_date = datetime.datetime.fromtimestamp(
+                os.path.getmtime(fl))
+            duration = today - modified_date
+            if duration.days > 7:
+                rmtree(fl)
