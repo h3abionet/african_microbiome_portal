9f683629ab112c12db81d485c89b5e529b584fbd
codemeleon <anmol.kiran@gmail.com>
2022-05-18 16:20:28 +0200

    To upload whole folder

--- upload.sh
+++ upload.sh
@@ -1,5 +1,13 @@
 #python manage.py upload_data --infile Data/refined_metadata_2_metadata_refined.csv
 #python manage.py upload_data --infile ../Database_bkp/Database/Data/test.csv
-python manage.py upload_data --infile \
-    ../Database_bkp/Database/Data/mgp12183.csv
+
+for fl in $(ls /home/devil/Documents/Tools/H3A/MicroBiomeDatabase/AMPData/Corrected/*.csv); do
+        echo $fl
+
+        python manage.py upload_data --infile $fl \
+                --elo_wiki_file /home/devil/Documents/Tools/H3A/MicroBiomeDatabase/AMPData/fixed/elo_wiki.csv \
+                --participant_summary_file /home/devil/Documents/Tools/H3A/MicroBiomeDatabase/AMPData/fixed/project_summary.csv
+        #break
+done
+#../Database_bkp/Database/Data/mgp12183.csv
 #python manage.py upload_data --infile Data/refined_metadata_1_12_03_Main.csv
