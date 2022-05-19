7460285353c6a5709c5387bae6091cb9c7401745
codemeleon <anmol.kiran@gmail.com>
2022-05-19 02:00:05 +0200

    Removal of automation and addition of double click link opening

--- MicroBiome/static/js/ajax.js
+++ MicroBiome/static/js/ajax.js
@@ -1,35 +1,43 @@
-function delay(callback, ms) {
-    var timer = 0;
-    return function() {
-        var context = this,
-            args = arguments;
-        clearTimeout(timer);
-        timer = setTimeout(function() {
-            callback.apply(context, args);
-        }, ms || 0);
-    };
-}
+//function delay(callback, ms) {
+//var timer = 0;
+//return function() {
+//var context = this, args = arguments;
+//clearTimeout(timer);
+//timer = setTimeout(function() { callback.apply(context, args); }, ms || 0);
+//};
+//}
 
 // https://stackoverflow.com/questions/1909441/how-to-delay-the-keyup-handler-until-the-user-stops-typing
 
-$(function() {
-    $("#search").keyup(
-        delay(function() {
-            $.ajax({
-                type: "POST",
-                url: "/results/",
-                data: {
-                    tags: $("#search").val(),
-                    oldsearch: $("#tags").val(),
-                    csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken").val(),
-                },
-                success: searchSuccess,
-                dataType: "html",
-            });
-        }, 2000)
-    ); // 2 seconds delay after after typing
-});
+//$(function () {
+//$("#search").keyup(
+//delay(function () {
+//$.ajax({
+//type: "POST",
+//url: "/results/",
+//data:
+//tags: $("#search").val(),
+//oldsearch: $("#tags").val(),
+//csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val(),
+//},
+//success: searchSuccess,
+//dataType: "html",
+//});
+//}, 2000)
+//); // 2 seconds delay after after typing
+//});
 
-function searchSuccess(data, textStatus, jqXHR) {
-    $("#dynamic").html(data);
-}
+//function searchSuccess(data, textStatus, jqXHR) { $("#dynamic").html(data); }
+
+// Code for douple click
+
+$(function () {
+  $(".doubleclick")
+    .click(function () {
+      return false;
+    })
+    .dblclick(function () {
+      window.location = this.href;
+      return false;
+    });
+});
