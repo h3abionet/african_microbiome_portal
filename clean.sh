91cd9f2f9f8557fac314a423e902fb25ff6b6598
codemeleon <anmol.kiran@gmail.com>
2022-05-21 11:48:21 +0200

    auto pull data and update

--- clean.sh
+++ clean.sh
@@ -4,3 +4,14 @@ rm db.sqlite3
 python manage.py makemigrations MicroBiome
 python manage.py migrate
 python manage.py su
+
+git clone https://github.com/codemeleon/AMPData
+
+for fl in $(ls /home/app/african_microbiome_portal/AMPData/Corrected/*.csv); do
+        echo $fl
+
+        python manage.py upload_data --infile $fl \
+                --elo_wiki_file /home/app/african_microbiome_portal/AMPData/fixed/elo_wiki.csv \
+                --participant_summary_file /home/app/african_microbiome_portal/AMPData/fixed/project_summary.csv
+        break
+done
--- upload.sh
+++ upload.sh
@@ -7,7 +7,7 @@ for fl in $(ls /home/devil/Documents/Tools/H3A/MicroBiomeDatabase/AMPData/Correc
         python manage.py upload_data --infile $fl \
                 --elo_wiki_file /home/devil/Documents/Tools/H3A/MicroBiomeDatabase/AMPData/fixed/elo_wiki.csv \
                 --participant_summary_file /home/devil/Documents/Tools/H3A/MicroBiomeDatabase/AMPData/fixed/project_summary.csv
-        #break
+        break
 done
 #../Database_bkp/Database/Data/mgp12183.csv
 #python manage.py upload_data --infile Data/refined_metadata_1_12_03_Main.csv
