8234f3b74e42d4e0da24bf10ac5b5d52f95433d6
codemeleon <anmol.kiran@gmail.com>
2022-07-01 13:05:52 +0200

    removal of update

--- clean.sh
+++ clean.sh
@@ -5,13 +5,13 @@ python manage.py makemigrations MicroBiome
 python manage.py migrate
 python manage.py su
 
-git clone https://github.com/codemeleon/AMPData
+#git clone https://github.com/codemeleon/AMPData
 
-for fl in $(ls /home/app/african_microbiome_portal/AMPData/Corrected/*.csv); do
-        echo $fl
+#for fl in $(ls /home/app/african_microbiome_portal/AMPData/Corrected/*.csv); do
+#echo $fl
 
-        python manage.py upload_data --infile $fl \
-                --elo_wiki_file /home/app/african_microbiome_portal/AMPData/fixed/elo_wiki.csv \
-                --participant_summary_file /home/app/african_microbiome_portal/AMPData/fixed/project_summary.csv
-        #break
-done
+#python manage.py upload_data --infile $fl \
+#--elo_wiki_file /home/app/african_microbiome_portal/AMPData/fixed/elo_wiki.csv \
+#--participant_summary_file /home/app/african_microbiome_portal/AMPData/fixed/project_summary.csv
+##break
+#done
