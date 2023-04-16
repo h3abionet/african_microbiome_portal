be773b04dd0b7a4f539233d4606d4bce5bd77ca5
codemeleon <anmol.kiran@gmail.com>
2023-04-16 15:07:17 +0100

    remove config

--- Database/settings.py
+++ Database/settings.py
@@ -23,13 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-import config
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config.SECRET_KEY
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config.DEBUG
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
